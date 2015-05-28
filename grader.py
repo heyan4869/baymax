50cbd1c10533024b64b5587a7d304344766903f9
Yan <heyan4869@gmail.com>
2015-05-28 14:35:12 +0800

    update with comment

--- grader.py
+++ grader.py
@@ -5,17 +5,18 @@ from math import *
 import parser
 
 def testGrader():
-    # answer should be extracted from the student's homework
-    # and return as a string
+    ## answer should be extracted from the student's homework
+    ## and return as a string
     # str = imageReader()
 
-    # currently we use a presigned string for testing the function
+    ## currently we use a presigned string for testing the function
     ocrresult = "12 + 5 - 2 * ( 7 - 1 ) = 5"
 
-    # split the answer and get the formula as well as the answer
+    ## split the answer and get the formula as well as the answer
+    # use partition instead of split on the string
     diffpart = ocrresult.partition("=")
 
-    # get the question and answer part
+    ## get the question and answer part
     questionpart = diffpart[0]
     question = parser.expr(questionpart).compile()
     correctresult = eval(question)
